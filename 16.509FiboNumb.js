99e34e69994118c1037be2733fb50cebfe9b6b5d
shaburi16 <shaburi16@gmail.com>
2026-01-18 18:40:57 +0530

    FibnocciNumber

---
+++ 16.509FiboNumb.js
@@ -0,0 +1,9 @@
+var fib = function(n) {
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+    let a = 0, b = 1;
+    for (let i = 2; i <= n; i++) {
+        [a, b] = [b, a + b];
+    }
+    return b;
+};
