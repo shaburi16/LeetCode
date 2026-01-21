1b4888e81a42e3492d54b76561fcccb7e16c94df
shaburi16 <shaburi16@gmail.com>
2026-01-21 23:29:45 +0530

    3315

---
+++ 19.MinBitwiseArray2.js
@@ -0,0 +1,10 @@
+
+var minBitwiseArray = function(nums) {
+    const ans =[];
+    for (let i = 0; i<nums.length;i++){
+        let n =nums[i];
+        if(n != 2) ans[i] = n - ((n + 1) & (-n - 1)) / 2
+        else ans[i] = -1
+    }
+    return ans
+};
