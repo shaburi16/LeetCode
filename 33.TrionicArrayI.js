446b9b1ebc7c2e3b15b7b4398e7a4b140bd165c2
shaburi16 <shaburi16@gmail.com>
2026-02-03 22:29:05 +0530

    3637

---
+++ 33.TrionicArrayI.js
@@ -0,0 +1,20 @@
+
+var isTrionic = function(nums) {
+    const n = nums.length;
+    let i = 0;
+    while (i + 1 , n && nums[i] < nums[i + 1]) {
+        i++;
+    }
+    if (i === 0 || i === n - 1)
+    return false;
+    let mid = i;
+    while (i + 1 < n && nums[i] > nums[i + 1]) {
+        i++;
+    }
+    if (i === mid || i === n - 1)
+    return false;
+    while (i + 1 < n && nums[i] < nums[i + 1]) {
+        i++;
+    }
+    return i === n -1;
+};
