4c69bfd2399c5232e3d787f979a36d3a337ee95c
shaburi16 <shaburi16@gmail.com>
2026-02-16 20:37:06 +0530

    190

---
+++ 45.ReverseBits.js
@@ -0,0 +1,9 @@
+let reverseBit = function(n){
+    result = 0;
+    for (let i = 0; i < 32; i++){
+        result = result << 1;
+        result = result | (n & 1);
+        n = n >>> 1;
+    }
+    return result >>> 0;
+}
