ae4886272d54ed18abbd327efd2e5c37ffb51a4f
shaburi16 <shaburi16@gmail.com>
2026-02-18 17:48:51 +0530

    693

---
+++ 47.BinaryaNumberAlternativeBits.js
@@ -0,0 +1,12 @@
+var hasAlternatingBits = function(n) {
+    let prev = n & 1;
+    n = n >> 1;
+
+    while (n > 0) {
+        let curr = n & 1;
+        if (curr === prev) return false;
+        prev = curr;
+        n = n >> 1;
+    }
+    return true;
+};
