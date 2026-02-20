da2bf4e819d7f7945481cfea7ab3238dbb98a3d6
shaburi16 <shaburi16@gmail.com>
2026-02-20 22:49:30 +0530

    761

---
+++ 49.SpecialBinaryString.js
@@ -0,0 +1,21 @@
+
+var makeLargestSpecial = function(s) {
+    let parts = [];
+    let count = 0;
+    let start = 0;
+    
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '1') count++;
+        else count--;
+        
+        if (count === 0) {
+            let inner = makeLargestSpecial(s.substring(start + 1, i));
+            parts.push("1" + inner + "0");
+            start = i + 1;
+        }
+    }
+    
+    parts.sort((a, b) => b.localeCompare(a));
+    
+    return parts.join('');
+};
