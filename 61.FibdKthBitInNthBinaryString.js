e641fe38e03f504fa0eb8e0cba002296dfd22954
shaburi16 <shaburi16@gmail.com>
2026-03-03 21:23:59 +0530

    1545

---
+++ 61.FibdKthBitInNthBinaryString.js
@@ -0,0 +1,18 @@
+
+var findKthBit = function(n, k) {
+    if (n === 1) return '0';
+
+    const length = (1 << n) - 1;
+    const mid = Math.floor((length + 1) / 2);
+
+    if (k === mid) {
+        return '1';
+    } 
+    else if (k < mid) {
+        return findKthBit(n - 1, k);
+    } 
+    else {
+        const bit = findKthBit(n - 1, length - k + 1);
+        return bit === '0' ? '1' : '0';
+    }
+};
