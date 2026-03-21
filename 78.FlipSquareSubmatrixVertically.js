d45a7998e9c7505634af041ba2aa51a98668cd38
shaburi16 <shaburi16@gmail.com>
2026-03-21 23:39:52 +0530

    3643

---
+++ 78.FlipSquareSubmatrixVertically.js
@@ -0,0 +1,13 @@
+var reverseSubmatrix = function(grid, x, y, k) {
+    for (let i = 0; i < Math.floor(k / 2); i++) {
+        let top = x + i;
+        let bottom = x + k - 1 - i;
+
+        for (let j = 0; j < k; j++) {
+            let temp = grid[top][y + j];
+            grid[top][y + j] = grid[bottom][y + j];
+            grid[bottom][y + j] = temp;
+        }
+    }
+    return grid;
+};
