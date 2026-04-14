bc10f06bc1b2d81e7154d363270555ce4c6bfb8d
shaburi16 <shaburi16@gmail.com>
2026-04-14 15:43:14 +0530

    1848

---
+++ 98.MiniDisToTheTargetEle.js
@@ -0,0 +1,11 @@
+var getMinDistance = function(nums, target, start) {
+    let answer = Infinity;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === target) {
+            answer = Math.min(answer, Math.abs(i - start));
+        }
+    }
+
+    return answer;
+};
