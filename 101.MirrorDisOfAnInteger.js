9828a684480e7eaf81f98b579b9509796eeccf93
shaburi16 <shaburi16@gmail.com>
2026-04-19 05:11:00 +0530

    3783

---
+++ 101.MirrorDisOfAnInteger.js
@@ -0,0 +1,11 @@
+var mirrorDistance = function (n) {
+    const getReverse = () => {
+        let rev = 0;
+        while (n > 0) {
+            rev = rev * 10 + n % 10
+            n = Math.floor(n / 10)
+        }
+        return rev
+    }
+    return Math.abs(n - getReverse(n))
+};
